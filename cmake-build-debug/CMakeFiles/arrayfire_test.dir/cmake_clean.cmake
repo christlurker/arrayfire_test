f761d5bf7fa85f16c020a48b357ce7651e5ffe02
christlurker <hzhang032@e.ntu.edu.sg>
2017-01-13 18:39:42 +0800

    first commit to test if arrayfire on cuda works

---
+++ cmake-build-debug/CMakeFiles/arrayfire_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arrayfire_test.dir/main.cpp.o"
+  "arrayfire_test.pdb"
+  "arrayfire_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arrayfire_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
